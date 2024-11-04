7e0bab51e1cb5cfdb2fc974213f2e119f9fdfd90
AnaJheloisa <ana.helo3b3@gmail.com>
2024-11-04 12:24:21 -0300

    primeiro commit

---
+++ SRC/components/Title/style.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    BoxTitle:{
+        alignItems:"center",
+
+
+    },
+
+    textTitle:{
+        color:"#EA3829",
+    },
+});
+
+export default styles
